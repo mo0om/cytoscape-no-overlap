e002581b43a1ef2800420412ae62d1a2a3ae8199
mheon <mheon@ciena.com>
2018-11-12 12:40:56 -0500

    Updates

--- cytoscape-no-overlap.js
+++ cytoscape-no-overlap.js
@@ -84,35 +84,76 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /**
- * Determines on which position to draw the node
- * (top, bottom, left, right)
- * @param {CytoscapeElement} node The node
- * @param {CytoscapeElement} node The node
- * @returns {string} Returns the position
+ * Verifies each points of the bounding box (a) and looks if any of these points are inside the second bounding box (b)
+ * @param {CytoscapeElement} a Node to compare
+ * @param {CytoscapeElement} b Node to compare
+ * @returns {boolean} Return true if node a overlaps node b
  */
-function findPosition(movingNode, fixedNode) {
-    var moving = movingNode.boundingBox();
-    var fixed = fixedNode.boundingBox();
-
-    var movingMiddleOfNode = {
-        x: moving.x1 + moving.w / 2,
-        y: moving.y1 + moving.h / 2
-    };
-    var fixedMiddleOfNode = {
-        x: fixed.x1 + fixed.w / 2,
-        y: fixed.y1 + fixed.h / 2
-    };
-    var finalPosition = '';
-    if (movingMiddleOfNode.y > fixedMiddleOfNode.y && Math.abs(movingMiddleOfNode.y - fixedMiddleOfNode.y) > Math.abs(movingMiddleOfNode.x - fixedMiddleOfNode.x)) {
-        finalPosition = 'bottom';
-    } else if (movingMiddleOfNode.y < fixedMiddleOfNode.y && Math.abs(movingMiddleOfNode.y - fixedMiddleOfNode.y) > Math.abs(movingMiddleOfNode.x - fixedMiddleOfNode.x)) {
-        finalPosition = 'top';
-    } else if (movingMiddleOfNode.x > fixedMiddleOfNode.x && Math.abs(movingMiddleOfNode.x - fixedMiddleOfNode.x) > Math.abs(movingMiddleOfNode.y - fixedMiddleOfNode.y)) {
-        finalPosition += 'right';
-    } else if (movingMiddleOfNode.x < fixedMiddleOfNode.x && Math.abs(movingMiddleOfNode.x - fixedMiddleOfNode.x) > Math.abs(movingMiddleOfNode.y - fixedMiddleOfNode.y)) {
-        finalPosition += 'left';
+function compareNodes(a, b) {
+    var isOverlapping = false;
+    // bottom right
+    if (a.x1 < b.x2 && a.x1 > b.x1 && a.y1 < b.y2 && a.y1 > b.y1) {
+        isOverlapping = true;
     }
-    return finalPosition;
+    // bottom left
+    if (a.x2 < b.x2 && a.x2 > b.x1 && a.y1 < b.y2 && a.y1 > b.y1) {
+        isOverlapping = true;
+    }
+    // top left
+    if (a.x2 < b.x2 && a.x2 > b.x1 && a.y2 > b.y1 && a.y2 < b.y2) {
+        isOverlapping = true;
+    }
+    // top right
+    if (a.x1 < b.x2 && a.x1 > b.x1 && a.y2 < b.y2 && a.y2 > b.y1) {
+        isOverlapping = true;
+    }
+    return isOverlapping;
+}
+
+/**
+ * Recursively checks if a node or it's parent overlaps any of the shown nodes
+ * @param {CytoscapeElement} node The node to verify
+ * @param {number} padding A bigger padding will make the overlap happen before
+ * @returns {boolean} True if the node overlaps any of the other nodes
+ */
+function checkIfOverlaps(node, padding) {
+    var siblings = void 0;
+    if (node.isChild()) {
+        siblings = node.parent().children().difference(node);
+    } else {
+        siblings = node.cy().nodes().orphans().difference(node);
+    }
+    var isOverlapping = false;
+    siblings.forEach(function (neighbor) {
+        var neighborBB = {
+            w: neighbor.renderedOuterWidth(),
+            h: neighbor.renderedOuterHeight(),
+            x1: neighbor.renderedPoint().x - neighbor.renderedOuterWidth() / 2 - padding,
+            x2: neighbor.renderedPoint().x + neighbor.renderedOuterWidth() / 2 + padding,
+            y1: neighbor.renderedPoint().y - neighbor.renderedOuterHeight() / 2 - padding,
+            y2: neighbor.renderedPoint().y + neighbor.renderedOuterHeight() / 2 + padding
+        };
+        var currentNodeBB = {
+            w: node.renderedOuterWidth(),
+            h: node.renderedOuterHeight(),
+            x1: node.renderedPoint().x - node.renderedOuterWidth() / 2 - padding,
+            x2: node.renderedPoint().x + node.renderedOuterWidth() / 2 + padding,
+            y1: node.renderedPoint().y - node.renderedOuterHeight() / 2 - padding,
+            y2: node.renderedPoint().y + node.renderedOuterHeight() / 2 + padding
+        };
+        if (compareNodes(currentNodeBB, neighborBB)) {
+            isOverlapping = true;
+        }
+        if (compareNodes(neighborBB, currentNodeBB)) {
+            isOverlapping = true;
+        }
+    });
+    if (node.parent().length > 0) {
+        if (checkIfOverlaps(node.parent(), padding)) {
+            isOverlapping = true;
+        }
+    }
+    return isOverlapping;
 }
 
 module.exports = function () {
@@ -120,88 +161,15 @@ module.exports = function () {
         _ref$padding = _ref.padding,
         padding = _ref$padding === undefined ? 0 : _ref$padding;
 
-    var eles = this;
-    var cy = this.cy();
-
-    eles.on('drag', function (evt) {
-        // boundingBox() is not returnin the right width and height
-        var currentPosition = {
-            x1: evt.target.position().x - evt.target.width() / 2,
-            x2: evt.target.position().x + evt.target.width() / 2,
-            y1: evt.target.position().y - evt.target.height() / 2,
-            y2: evt.target.position().y + evt.target.height() / 2,
-            w: evt.target.width(),
-            h: evt.target.height()
-        };
-        var overlappingNode = null;
-
-        cy.nodes().forEach(function (aNode) {
-            if (aNode.id() === evt.target.id()) {
-                return;
-            }
-            var bb = JSON.parse(JSON.stringify(aNode.boundingbox()));
-            bb.x1 -= padding;
-            bb.x2 += padding;
-            bb.y1 -= padding;
-            bb.y2 += padding;
-            // bottom right
-            if (currentPosition.x1 < bb.x2 && currentPosition.x1 > bb.x1 && currentPosition.y1 < bb.y2 && currentPosition.y1 > bb.y1) {
-                overlappingNode = aNode;
-            }
-            // bottom left
-            if (currentPosition.x2 < bb.x2 && currentPosition.x2 > bb.x1 && currentPosition.y1 < bb.y2 && currentPosition.y1 > bb.y1) {
-                overlappingNode = aNode;
-            }
-            // top left
-            if (currentPosition.x2 < bb.x2 && currentPosition.x2 > bb.x1 && currentPosition.y2 > bb.y1 && currentPosition.y2 < bb.y2) {
-                overlappingNode = aNode;
-            }
-            // top right
-            if (currentPosition.x1 < bb.x2 && currentPosition.x1 > bb.x1 && currentPosition.y2 < bb.y2 && currentPosition.y2 > bb.y1) {
-                overlappingNode = aNode;
-            }
-        });
-        if (overlappingNode) {
-            var position = findPosition(evt.target, overlappingNode);
-            var overlappingBB = JSON.parse(JSON.stringify(overlappingNode.boundingBox()));
-            overlappingBB.x1 -= padding;
-            overlappingBB.x2 += padding;
-            overlappingBB.y1 -= padding;
-            overlappingBB.y2 += padding;
-
-            var finalPos = { x: 0, y: 0 };
-            switch (position) {
-                case 'top':
-                    finalPos = {
-                        x: currentPosition.x1 + currentPosition.w / 2,
-                        y: overlappingBB.y1 - currentPosition.h / 2
-                    };
-                    break;
-                case 'right':
-                    finalPos = {
-                        x: overlappingBB.x2 + currentPosition.w / 2,
-                        y: currentPosition.y1 + currentPosition.w / 2
-                    };
-                    break;
-                case 'bottom':
-                    finalPos = {
-                        x: currentPosition.x1 + currentPosition.w / 2,
-                        y: overlappingBB.y2 + currentPosition.h / 2
-                    };
-                    break;
-                case 'left':
-                    finalPos = {
-                        x: overlappingBB.x1 - currentPosition.w / 2,
-                        y: currentPosition.y1 + currentPosition.h / 2
-                    };
-                    break;
-            }
-            evt.target.position(finalPos);
-            return;
+    this.on('drag', function (evt) {
+        if (checkIfOverlaps(evt.target, padding)) {
+            evt.target.position(evt.target.scratch('previousPosition'));
+        } else {
+            evt.target.scratch('previousPosition', JSON.parse(JSON.stringify(evt.target.position())));
         }
     });
 
-    return this; // chainability
+    return this;
 };
 
 /***/ }),
@@ -224,7 +192,7 @@ var register = function register(cytoscape) {
 
 if (typeof cytoscape !== 'undefined') {
   // expose to global cytoscape (i.e. window.cytoscape)
-  register(cytoscape);
+  register(window.cytoscape);
 }
 
 module.exports = register;
